f045834defe8d14e90cc931b98212a51c41db7cc
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-10-30 20:54:22 -0500

    Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,46 @@
+lista_dias = ["lunes","martes","miércoles","jueves","viernes","sábado","domingo"]
+dia = input("Ingrese el día: ").lower() #lunes
+codigo = input("Ingrese el código: ") #"45251"
+monto = float(input("Ingrese el monto: ")) # 300
+
+acum_desc = 0
+
+#Descuento 1: Ultimo dígito
+num_dia = lista_dias.index(dia) + 1 # 1
+num_dia_tarj = int(codigo[-1]) #1
+if num_dia == num_dia_tarj:
+    acum_desc += 5
+    
+#Descuento 2: Penúltimo dígito
+penult_digito = int(codigo[-2]) #5
+if monto >300:
+    acum_desc += penult_digito
+
+#Descuento 3: Antepenúltimo dígito 
+antpenult_digito = int(codigo[-3])
+if 400 < monto <= 500: #monto > 400 and monto <=500
+    acum_desc += antpenult_digito
+    
+#Descuento 4: Compra mayor a 500
+if monto > 500:
+    acum_desc += 10
+    
+    
+print('acum_desc: ', acum_desc) 
+monto_total = monto - (monto * acum_desc/100)
+print('El monto con descuentos es: ', monto_total)
+    
+
+    
+    
+    
+
+
+
+    
+
+
+
+
+
+
